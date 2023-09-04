c7b69555d681185b3072ec17fed9593fad631148
AleksSkyr <greshnik1499@gmail.com>
2023-09-04 18:24:55 +0300

    test 2.0

--- src/main/java/Service/CalculatorService.java
+++ src/main/java/Service/CalculatorService.java
@@ -6,23 +6,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
 
-    public Number plus(Integer a, Integer b) {
-        return a + b;
+    public Number plus(double num1, double num2) {
+        return num1 + num2;
     }
 
-    public Number minus(Integer a, Integer b) {
-        return a - b;
+    public Number minus(double num1, double num2) {
+        return num1 - num2;
     }
 
-    public Number multiply(Integer a, Integer b) {
-        return a * b;
+    public Number multiply(double num1, double num2) {
+        return num1 * num2;
     }
 
-    public Number divide(Integer a, Integer b) {
-        if (b == 0) {
-            throw new DivideByZeroException("Делить на 0 нельзя!");
+    public Number divide(double num1, double num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Делить на 0 нельзя!");
         }
-        return a.doubleValue() / b;
+        return num1 / num2;
     }
 
 }
--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -13,56 +13,35 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
-
-    private final CalculatorService calculatorService;
-
-    public CalculatorController(CalculatorService calculatorService) {
-        this.calculatorService = calculatorService;
-    }
-
-    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
-    public String greetings() {
-        return "<h1>Добро пожаловать в калькулятор!</h1>";
+    private final CalculatorService calculator = new CalculatorService();
+    @GetMapping
+    public String hello() {
+        return "<h3>Добро пожаловать калькулятор</h3>";
     }
 
     @GetMapping("/plus")
-    public String plus(@RequestParam(value = "num1", required = false) Integer a,
-                       @RequestParam(value = "num2", required = false) Integer b) {
-        if (Objects.isNull(a) || Objects.isNull(b)) {
-            return "Оба параметра должны быть переданы!";
-        }
-        return buildResult(a, b, calculatorService.plus(a, b), "+");
+    public String plus(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + " + " + num2 + " = " + calculator.plus(num1, num2);
     }
 
     @GetMapping("/minus")
-    public String minus(@RequestParam(value = "num1", required = false) Integer a,
-                        @RequestParam(value = "num2", required = false) Integer b) {
-        if (Objects.isNull(a) || Objects.isNull(b)) {
-            return "Оба параметра должны быть переданы!";
-        }
-        return buildResult(a, b, calculatorService.minus(a, b), "-");
+    public String minus(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + " - " + num2 + " = " + calculator.minus(num1, num2);
     }
 
     @GetMapping("/multiply")
-    public String multiply(@RequestParam(value = "num1", required = false) Integer a,
-                           @RequestParam(value = "num2", required = false) Integer b) {
-        if (Objects.isNull(a) || Objects.isNull(b)) {
-            return "Оба параметра должны быть переданы!";
+    public String multiply(@RequestParam double num1, @RequestParam double num2) {
+        if (num2 == 0) {
+            return "Умножать на 0 нельзя";
         }
-        return buildResult(a, b, calculatorService.multiply(a, b), "*");
+        return num1 + " * " + num2 + " = " + calculator.multiply(num1, num2);
     }
 
     @GetMapping("/divide")
-    public String divide(@RequestParam(value = "num1", required = false) Integer a,
-                         @RequestParam(value = "num2", required = false) Integer b) {
-        if (Objects.isNull(a) || Objects.isNull(b)) {
-            return "Оба параметра должны быть переданы!";
+    public String divide(@RequestParam double num1, @RequestParam double num2) {
+        if (num2 == 0) {
+            return "Делить на 0 нельзя";
         }
-        return buildResult(a, b, calculatorService.divide(a, b), "/");
+        return num1 + " / " + num2 + " = " + calculator.divide(num1, num2);
     }
-
-    private String buildResult(Number a, Number b, Number result, String operation) {
-        return String.format("%s %s %s = %s", a, operation, b, result);
-    }
-
 }
