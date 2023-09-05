7c203d32eacbd397233420de7d5f43ae3d54363d
AleksSkyr <greshnik1499@gmail.com>
2023-09-05 20:33:18 +0300

    3

--- src/test/java/Service/CalculatorServiceTest.java
+++ src/test/java/Service/CalculatorServiceTest.java
@@ -49,19 +49,19 @@ class CalculatorServiceTest {
 
     @Test
     void testMultiply() {
-        var actual = service.plus(5d, 5d);
+        var actual = service.multiply(5d, 5d);
         assertEquals(25d, actual);
 
-        var actual1 = service.plus(-2d, -3d);
+        var actual1 = service.multiply(-2d, -3d);
         assertEquals(1d, actual1);
 
-        var actual2 = service.plus(2d, -3d);
+        var actual2 = service.multiply(2d, -3d);
         assertEquals(5d, actual2);
 
-        var actual3 = service.plus(-2d, 3d);
+        var actual3 = service.multiply(-2d, 3d);
         assertEquals(-5d, actual3);
 
-        var actual4 = service.plus(-2d, 0d);
+        var actual4 = service.multiply(-2d, 0d);
         assertEquals(-2d, actual4);
 
 
@@ -69,22 +69,22 @@ class CalculatorServiceTest {
 
     @Test
     void testDivide() {
-        var actual = service.plus(5d, 5d);
+        var actual = service.divide(5d, 5d);
         assertEquals(1d, actual);
 
-        var actual1 = service.plus(-3d, -3d);
+        var actual1 = service.divide(-3d, -3d);
         assertEquals(-6d, actual1);
 
-        var actual2 = service.plus(5d, 5d);
+        var actual2 = service.divide(5d, 5d);
         assertEquals(1d, actual2);
 
-        var actual3 = service.plus(-2d, 4d);
+        var actual3 = service.divide(-2d, 4d);
         assertEquals(2d, actual3);
 
-        var actual4 = service.plus(-2d, 0d);
+        var actual4 = service.divide(-2d, 0d);
         assertEquals(2d, actual4);
 
-        var actual5 = service.plus(2d, 0d);
+        var actual5 = service.divide(2d, 0d);
         assertEquals(0d, actual5);
 
         assertThrows(IllegalAccessError.class, () -> service.divide(7d, 0d));
